0cf769a95753e98cb31f5e6d7762bc61a5d031d5
TEAM CASABA <151868415+Cobopanther@users.noreply.github.com>
2026-09-15 18:37:32 +0530

    Update execom list to strictly 9 members with exact roles

--- src/lib/execomData.js
+++ src/lib/execomData.js
@@ -1,33 +1,15 @@
-// Local Execom Data using photos from src/ieee
-import abhikrishnaImg from "../ieee/abhikrishna.png";
-import achuthImg from "../ieee/achuth.png";
-import adhithiyaImg from "../ieee/adhithiya.png";
-import agnusImg from "../ieee/agnus.png";
-import amithaImg from "../ieee/amitha.png";
-import anandImg from "../ieee/anand.png";
-import anandhapadmanbhanImg from "../ieee/anandhapadmanbhan.png";
-import angelImg from "../ieee/angel.png";
-import annImg from "../ieee/ann.png";
-import aryanImg from "../ieee/aryan.png";
-import ashtamiImg from "../ieee/ashtami.png";
-import ayushImg from "../ieee/ayush.png";
-import basilImg from "../ieee/basil.png";
-import hishamImg from "../ieee/hisham.png";
-import jibinImg from "../ieee/jibin.png";
-import joelImg from "../ieee/joel.png";
-import joshiniImg from "../ieee/joshini.png";
-import joveetaImg from "../ieee/joveeta.png";
-import kashiImg from "../ieee/kashi.png";
-import mishalImg from "../ieee/mishal.png";
-import reneeshImg from "../ieee/reneesh.png";
-import rittuImg from "../ieee/rittu.png";
-import roshiniImg from "../ieee/roshini.png";
-import rubenImg from "../ieee/ruben.png";
-import sreyaImg from "../ieee/sreya.png";
+// Execom Members Data using photos from src/ieee
 import sreyasImg from "../ieee/sreyas.png";
+import joveetaImg from "../ieee/joveeta.png";
+import annImg from "../ieee/ann.png";
+import agnusImg from "../ieee/agnus.png";
+import rubenImg from "../ieee/ruben.png";
+import amithaImg from "../ieee/amitha.png";
+import mishalImg from "../ieee/mishal.png";
+import anandImg from "../ieee/anand.png";
+import roshiniImg from "../ieee/roshini.png";
 
 export const execomMembers = [
-    // --- 10 Executive Committee Members ---
     {
         id: "1",
         name: "Sreyas Kumar A",
@@ -50,8 +32,8 @@ export const execomMembers = [
     },
     {
         id: "3",
-        name: "Ann P. Mathew",
-        role: "SB Vice Chair",
+        name: "Ann P Mathew",
+        role: "IEEE Vice Chair",
         image: annImg,
         instagram: "https://www.instagram.com/_ann__rachel_?stkn=MXFnd2phMWlmanllaA==",
         github: "https://github.com/annpmathew21-debug",
@@ -61,7 +43,7 @@ export const execomMembers = [
     {
         id: "4",
         name: "Agnus Varghese",
-        role: "CS Chair",
+        role: "IEEE CS Chair",
         image: agnusImg,
         instagram: "https://www.instagram.com/__agnus__varghese__?stkn=OWdrNWQ1d2Nhbnl2",
         github: null,
@@ -71,7 +53,7 @@ export const execomMembers = [
     {
         id: "5",
         name: "Ruben Koshy",
-        role: "Computer Society Secretary,\nLink Representative",
+        role: "IEEE CS Secretary",
         image: rubenImg,
         instagram: "https://www.instagram.com/_its_me_ruben_?stkn=MzljaGljdDhsZmk2",
         github: "https://github.com/Ruben-15",
@@ -81,7 +63,7 @@ export const execomMembers = [
     {
         id: "6",
         name: "Amitha Jom",
-        role: "Computer Society Vice Chair, MDC",
+        role: "IEEE Vice Chair",
         image: amithaImg,
         instagram: "https://www.instagram.com/ami_tha_jom?stkn=MWloeG11Y3g0dXNicQ==",
         github: null,
@@ -90,8 +72,8 @@ export const execomMembers = [
     },
     {
         id: "7",
-        name: "Muhammad Mishal Shihab",
-        role: "SB Treasurer",
+        name: "Mishal Bin Shihab",
+        role: "Treasurer",
         image: mishalImg,
         instagram: "https://www.instagram.com/_mishal_bin_shihab_?stkn=OHRzMHc3dmdnbnF4",
         github: null,
@@ -111,184 +93,12 @@ export const execomMembers = [
     {
         id: "9",
         name: "Fathima Roshini Siyad",
-        role: "Women In Computing",
+        role: "WIC",
         image: roshiniImg,
         instagram: "https://www.instagram.com/roshnii.e_?stkn=MW9zdW5kOW02MXZhNA==",
         github: "https://github.com/FATHIMA-ROSHINI",
         linkedin: "https://www.linkedin.com/in/fathima-roshini-siyad-8477292b3",
         email: "roshni.m.k.s20@gmail.com",
-    },
-    {
-        id: "10",
-        name: "Renesh Rajesh",
-        role: "Project and Technical Coordinator",
-        image: reneeshImg,
-        instagram: "https://www.instagram.com/re._.nesh?stkn=MTFzdXRiNWlpZzZieA==",
-        github: "https://github.com/renesh-42",
-        linkedin: "https://www.linkedin.com/in/renesh-rajesh-b853a8280",
-        email: "reneshrajesh487@gmail.com",
-    },
-
-    // --- Additional Members (Easy to edit & customize) ---
-    {
-        id: "11",
-        name: "Abhikrishna",
-        role: "Executive Member",
-        image: abhikrishnaImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "12",
-        name: "Achuth",
-        role: "Executive Member",
-        image: achuthImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "13",
-        name: "Adhithiya",
-        role: "Executive Member",
-        image: adhithiyaImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "14",
-        name: "Anandhapadmanabhan",
-        role: "Executive Member",
-        image: anandhapadmanbhanImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "15",
-        name: "Angel",
-        role: "Executive Member",
-        image: angelImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "16",
-        name: "Aryan",
-        role: "Executive Member",
-        image: aryanImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "17",
-        name: "Ashtami",
-        role: "Executive Member",
-        image: ashtamiImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "18",
-        name: "Ayush",
-        role: "Executive Member",
-        image: ayushImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "19",
-        name: "Basil",
-        role: "Executive Member",
-        image: basilImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "20",
-        name: "Hisham",
-        role: "Executive Member",
-        image: hishamImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "21",
-        name: "Jibin",
-        role: "Executive Member",
-        image: jibinImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "22",
-        name: "Joel",
-        role: "Executive Member",
-        image: joelImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "23",
-        name: "Joshini",
-        role: "Executive Member",
-        image: joshiniImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "24",
-        name: "Kashi",
-        role: "Executive Member",
-        image: kashiImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "25",
-        name: "Rittu",
-        role: "Executive Member",
-        image: rittuImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
-    },
-    {
-        id: "26",
-        name: "Sreya",
-        role: "Executive Member",
-        image: sreyaImg,
-        instagram: null,
-        github: null,
-        linkedin: null,
-        email: null,
     },
 ];
 
